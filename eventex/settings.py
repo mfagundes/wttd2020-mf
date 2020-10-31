f736202679c1f50eda1e24d204bcd94283fe4890
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2020-10-30 21:38:58 -0300

    Inicializa admin e cria Subscription Admin

--- eventex/settings.py
+++ eventex/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'test_without_migrations',
     'eventex.core',
-    'eventex.subscriptions'
+    'eventex.subscriptions.apps.SubscriptionsConfig'
 ]
 
 MIDDLEWARE = [
