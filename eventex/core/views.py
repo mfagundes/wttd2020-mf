77a7043ab2114857a239484f77b120db44c09278
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2021-01-24 18:01:58 -0300

    Keynote speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'speakers': [
+            {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+            {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+        ]
+    }
+
+    return render(request, 'index.html', context)
